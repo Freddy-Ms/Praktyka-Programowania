c2e3fcc45e4aa7aa71c7938e95c3822a861e5976
Freddy-Ms <maciejskrzypa2705@gmail.com>
2025-05-13 14:13:12 +0200

    docstringi

--- utils.py
+++ utils.py
@@ -1,9 +1,19 @@
 # przykladowe funkcje utils .py
+
+"""Funkcje do obliczen matematycznych"""
+
+"""Funkcja dodajaca dwie liczby"""
 def add ( a: int , b: int) -> int:
     return a + b
+
+"""Funkcja odejmujaca dwie liczby"""
 def subtract ( a: int , b: int) -> int:
     return a - b
+
+"""Funkcja mnozaca dwie liczby"""
 def multiply ( a: int , b: int) -> int:
     return a * b
+
+"""Funkcja dzielaca dwie liczby"""
 def divide ( a: int , b: int) -> float :
     return a / b
