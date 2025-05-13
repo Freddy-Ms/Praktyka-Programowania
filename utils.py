b40ff81d14bb1d270d1e025f167d3e52fa763d0a
Freddy-Ms <maciejskrzypa2705@gmail.com>
2025-05-13 14:19:51 +0200

    go

--- utils.py
+++ utils.py
@@ -1,17 +1,17 @@
 """Funkcje do obliczen matematycznych"""
 
-def add ( a: int , b: int) -> int:
+def add (a: int, b: int) -> int:
     """Funkcja dodajaca dwie liczby"""
     return a + b
 
-def subtract ( a: int , b: int) -> int:
+def subtract (a: int, b: int) -> int:
     """Funkcja odejmujaca dwie liczby"""
     return a - b
 
-def multiply ( a: int , b: int) -> int:
+def multiply (a: int, b: int) -> int:
     """Funkcja mnozaca dwie liczby"""
     return a * b
 
-def divide ( a: int , b: int) -> float:
+def divide (a: int, b: int) -> float:
     """Funkcja dzielaca dwie liczby"""
     return a / b
